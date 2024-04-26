39abfb661602859cfccbf7c1994801c2a273f45e
ArnoldRamodike <riccoarnold23@gmail.com>
2024-04-26 15:06:05 +0200

    Initial Layout

---
+++ app/_components/Hero.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+      <section className="bg-gray-50">
+        <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+          <div className="mx-auto max-w-xl text-center">
+            <h1 className="text-3xl font-extrabold sm:text-5xl">
+              GeniusGrove.
+              <strong className="font-extrabold text-primary sm:block"> Marketplace. </strong>
+            </h1>
+
+            <p className="mt-4 sm:text-xl/relaxed">
+              Learn everything fast to reach your dreams and goals faster than the traditional ways!
+            </p>
+
+            <div className="mt-8 flex flex-wrap justify-center gap-4">
+              <a
+                className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-blue-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+                href="#"
+              >
+                Get Started
+              </a>
+
+              <a
+                className="block w-full rounded px-12 py-3 text-sm font-medium text-tertiary shadow hover:text-blue-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
+                href="#"
+              >
+                Learn More
+              </a>
+            </div>
+          </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
