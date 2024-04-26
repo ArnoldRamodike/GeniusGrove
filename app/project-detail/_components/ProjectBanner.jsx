ebf16da4a55d774468e0dccc50bda503b32775b6
ArnoldRamodike <riccoarnold23@gmail.com>
2024-04-26 17:56:26 +0200

    Breadcrumb and Skeleton Components

--- app/project-detail/_components/ProjectBanner.jsx
+++ app/project-detail/_components/ProjectBanner.jsx
@@ -4,7 +4,9 @@ import React from 'react'
 const ProjectBanner = ({product}) => {
   return (
     <div>
-        <Image src={product?.attributes?.banner?.data?.attributes?.url} alt='banner' width={350} height={400} className='rounded-lg object-cover'/>
+    { product? <Image src={product?.attributes?.banner?.data?.attributes?.url} alt='banner' width={350} height={400} className='rounded-lg object-cover'/>
+       :<div className="h-[350px] w-[350px] bg-slate-200 animate-pulse"></div>
+    }
     </div>
   )
 }
--- app/project-detail/_components/ProjectInfo.jsx
+++ app/project-detail/_components/ProjectInfo.jsx
@@ -2,14 +2,23 @@ import { ShoppingCart } from 'lucide-react';
 import React from 'react'
 
 const ProjectInfo = ({product}) => {
-    console.log(product);
   return (
     <div>
-        <h2 className="text-[20px]">{product?.attributes?.title}</h2>
-        <h2 className="text-[15px] text-gray-400">{product?.attributes?.product_cat?.data?.attributes?.Name}</h2>
-        <h2 className="text-[12px] mt-5 text-gray-700">{product?.attributes?.description}</h2>
-        <h2 className="text-[35px] font-medium mt-5 text-primary">${product?.attributes?.pricing}</h2>
-        <button className='flex gap-2 p-3 bg-primary text-white rounded-lg px-10 mt-5 hover:bg-blue-700'><ShoppingCart/> Add to Cart</button>
+     {product?   
+        (<div>
+            <h2 className="text-[20px]">{product?.attributes?.title}</h2>
+            <h2 className="text-[15px] text-gray-400">{product?.attributes?.product_cat?.data?.attributes?.Name}</h2>
+            <h2 className="text-[12px] mt-5 text-gray-700">{product?.attributes?.description}</h2>
+            <h2 className="text-[35px] font-medium mt-5 text-primary">${product?.attributes?.pricing}</h2>
+            <button className='flex gap-2 p-3 bg-primary text-white rounded-lg px-10 mt-5 hover:bg-blue-700'><ShoppingCart/> Add to Cart</button>
+        </div>)
+        :( <div>
+               <div className="h-[20px] w-[300px] bg-slate-200 animate-pulse"></div>
+               <div className="h-[20px] w-[100px] bg-slate-200 animate-pulse"></div>
+               <div className="h-[20px] w-[400px] bg-slate-200 animate-pulse"></div>
+               <div className="h-[20px] w-[100px] bg-slate-200 animate-pulse"></div> 
+         </div>)
+    }
     </div>
   )
 }
